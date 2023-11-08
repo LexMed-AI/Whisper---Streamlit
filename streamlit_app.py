beb04e8b96e1c072b3f2a85f0cba95f6b869773b
flyhogz <45820958+flyhogz@users.noreply.github.com>
2023-11-08 15:08:08 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,44 @@
 import streamlit as st
 import requests
+import json
 from requests_toolbelt.multipart.encoder import MultipartEncoder
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+from io import BytesIO
+
+# Function to convert SRT text to PDF
+def srt_to_pdf(srt_text, file_name):
+    pdf_buffer = BytesIO()
+    c = canvas.Canvas(pdf_buffer, pagesize=letter)
+    c.setTitle(file_name)
+
+    y_position = 750
+    for line in srt_text.split('\n'):
+        if not line.strip().isdigit() and line.strip() != '':
+            c.drawString(72, y_position, line)
+            y_position -= 15
+            if y_position < 72:
+                y_position = 750
+                c.showPage()
+
+    c.save()
+    pdf_buffer.seek(0)
+    return pdf_buffer
 
 st.title('SSD Hearing Transcription')
 
+# Streamlit file uploader
 uploaded_file = st.file_uploader("Upload your OGG audio file", type=['ogg'])
 
 if uploaded_file is not None:
+    # Read the file content
+    file_content = uploaded_file.read()
+
     m = MultipartEncoder(
         fields={
-            'file': (uploaded_file.name, uploaded_file, 'application/ogg'),
-            'model': 'whisper-1'
+            'file': (uploaded_file.name, file_content, 'application/ogg'),
+            'model': 'whisper-1',
+            'response_format': 'srt'  # Request timestamps in SRT format
         }
     )
 
@@ -27,9 +55,28 @@ if uploaded_file is not None:
         headers=headers
     )
 
-    if response.status_code == 200:
-        st.write("Transcription completed successfully!")
-        transcription = response.json()['text']
-        st.text_area("Transcription", transcription)
+    # Debugging step: print out the raw response content for debugging
+    # st.write("Raw response content:", response.text)  # Uncomment this line for debugging
+
+    if response.status_code != 200:
+        # Print the error to the Streamlit interface
+        st.error(f"Failed to transcribe audio. Status code: {response.status_code}. Response text: {response.text}")
     else:
-        st.error("Failed to transcribe audio. Please try again.")
+        # Try to parse the JSON response and extract the transcription text
+        try:
+            srt_transcription = response.json()['text']
+            
+            # Convert the SRT text to a PDF
+            pdf_file_buffer = srt_to_pdf(srt_transcription, 'Transcription.pdf')
+            
+            # Let the user download the PDF
+            st.download_button(
+                label="Download Transcript as PDF",
+                data=pdf_file_buffer,
+                file_name='Transcription.pdf',
+                mime='application/pdf'
+            )
+            st.write("Transcription completed successfully!")
+        except json.JSONDecodeError:
+            st.error("Failed to parse the response as JSON.")
+            st.text(response.text)
