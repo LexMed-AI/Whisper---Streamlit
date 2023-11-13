7d76d6223bb194ad4a1e809c23e1dcb9ef55259c
flyhogz <45820958+flyhogz@users.noreply.github.com>
2023-11-13 11:56:12 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,106 +1,129 @@
 import streamlit as st
 from PIL import Image
-import requests
+from io import BytesIO
+import zipfile
+import os
 import json
-from requests_toolbelt.multipart.encoder import MultipartEncoder
+from bs4 import BeautifulSoup  # Import BeautifulSoup for parsing HTML
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
-from io import BytesIO
+import requests
+from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 # Load and display the logo
+
 logo_image = Image.open('lexmed_logo.png')
 st.image(logo_image, width=800)
 st.title('Hearing Whisperer')
 
 # Function to add speaker labels to the transcript
+
 def add_speaker_labels(transcript):
-    lines = transcript.split('\n')
-    processed_lines = []
-    speaker_cues = {
-        "claimant": ["I believe", "My condition", "I cannot", "hurt"],
-        "attorney": ["My client", "Objection", "No objections"],
-        "ALJ": ["Hearing will come to order", "Please state your name", "What is your opinion", "Thank you"],
-        "VE": ["In my professional opinion", "DOT Code", "Given these restrictions", "national economy"],
-        "ME": ["Medically speaking", "From a medical standpoint"]
-    }
-    current_speaker = None
-    for line in lines:
-        found_cue = False
-        for speaker, cues in speaker_cues.items():
-            if any(cue in line for cue in cues):
-                current_speaker = speaker
-                found_cue = True
-                break
-        if found_cue:
-            line = f"[{current_speaker.upper()}]: {line}"
-        else:
-            line = f"[UNKNOWN]: {line}"  # Mark lines where the speaker is unknown
-        processed_lines.append(line)
-    return '\n'.join(processed_lines)
+    # [Existing code for add_speaker_labels remains unchanged]
 
 # Function to convert SRT text to PDF
-def srt_to_pdf(srt_text, file_name):
-    pdf_buffer = BytesIO()
-    c = canvas.Canvas(pdf_buffer, pagesize=letter)
-    c.setTitle(file_name)
 
-    y_position = 750
-    for line in srt_text.split('\n'):
-        if not line.strip().isdigit() and line.strip() != '':
-            c.drawString(72, y_position, line)
-            y_position -= 15
-            if y_position < 72:
-                y_position = 750
-                c.showPage()
-    
-    c.save()
-    pdf_buffer.seek(0)
-    return pdf_buffer
+def srt_to_pdf(srt_text, file_name):
+    # [Existing code for srt_to_pdf remains unchanged]
+
+# Function to extract metadata from zip file
+
+def extract_metadata_from_index(zip_file):
+    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+        index_file = [f for f in zip_ref.namelist() if 'index.html' in f]
+        if index_file:
+            with zip_ref.open(index_file[0]) as file:
+                soup = BeautifulSoup(file, 'html.parser')
+                # Extract required metadata from the HTML
+                # This needs to be adjusted based on the structure of your HTML file
+                metadata = {}
+                # Example: metadata['title'] = soup.title.string
+                # Add more extraction logic as per your HTML structure
+                return metadata
+        else:
+            return None
 
 # Streamlit file uploader
-uploaded_file = st.file_uploader("Upload your OGG audio file", type=['ogg'])
+
+uploaded_file = st.file_uploader("Upload your Zip or OGG audio file", type=['zip', 'ogg'])
 
 if uploaded_file is not None:
-    # Read the file content
-    file_content = uploaded_file.read()
+    if uploaded_file.name.endswith('.zip'):
+        # Process Zip file
+        metadata = extract_metadata(uploaded_file)
+        if metadata:
+            st.write("Metadata extracted:", metadata)
+        else:
+            st.error("No metadata file found in the zip.")
 
-    m = MultipartEncoder(
-        fields={
-            'file': (uploaded_file.name, file_content, 'application/ogg'),
-            'model': 'whisper-1',
-            'response_format': 'srt'
+        # Assuming you need to extract an OGG file from the zip
+        with zipfile.ZipFile(uploaded_file, 'r') as zip_ref:
+            ogg_files = [f for f in zip_ref.namelist() if f.endswith('.ogg')]
+            if ogg_files:
+                ogg_file_path = ogg_files[0]
+                zip_ref.extract(ogg_file_path)
+                audio_file = open(ogg_file_path, 'rb')
+                # Process the OGG file (same as below)
+                # [Insert OGG file processing code here]
+                os.remove(ogg_file_path)  # Clean up extracted file
+            else:
+                st.error("No OGG audio file found in the zip.")
+
+    elif uploaded_file.name.endswith('.ogg'):
+        # Process OGG file directly
+        file_content = uploaded_file.read()
+
+        m = MultipartEncoder(
+            fields={
+                'file': (uploaded_file.name, file_content, 'application/ogg'),
+                'model': 'whisper-1',
+                'response_format': 'srt'
+            }
+        )
+
+        headers = {
+            'Authorization': f'Bearer {st.secrets["OPENAI_API_KEY"]}',
+            'Content-Type': m.content_type
         }
-    )
 
-    headers = {
-        'Authorization': f'Bearer {st.secrets["OPENAI_API_KEY"]}',
-        'Content-Type': m.content_type
-    }
+        st.write("Processing the audio file...")
 
-    st.write("Processing the audio file...")
+        response = requests.post(
+            url='<https://api.openai.com/v1/audio/transcriptions>',
+            data=m,
+            headers=headers
+        )
 
-    response = requests.post(
-        url='https://api.openai.com/v1/audio/transcriptions',
-        data=m,
-        headers=headers
-    )
+        if response.status_code != 200:
+            st.error(f"Failed to transcribe audio. Status code: {response.status_code}. Response text: {response.text}")
+        else:
+            try:
+                srt_transcription = response.json()['text']
+                srt_transcription_labeled = add_speaker_labels(srt_transcription)
+                pdf_file_buffer = srt_to_pdf(srt_transcription_labeled, 'Transcription.pdf')
 
-    if response.status_code != 200:
-        st.error(f"Failed to transcribe audio. Status code: {response.status_code}. Response text: {response.text}")
+                st.download_button(
+                    label="Download Transcript as PDF",
+                    data=pdf_file_buffer,
+                    file_name='Transcription.pdf',
+                    mime='application/pdf'
+                )
+                st.write("Transcription completed successfully!")
+            except json.JSONDecodeError:
+                st.error("Failed to parse the response as JSON.")
+                st.text(response.text)
+
     else:
-        try:
-            srt_transcription = response.json()['text']
-            # Add labels to speakers in the transcript
-            srt_transcription_labeled = add_speaker_labels(srt_transcription)
-            pdf_file_buffer = srt_to_pdf(srt_transcription_labeled, 'Transcription.pdf')
+        st.error("Unsupported file format.")
 
-            st.download_button(
-                label="Download Transcript as PDF",
-                data=pdf_buffer,
-                file_name='Transcription.pdf',
-                mime='application/pdf'
-            )
-            st.write("Transcription completed successfully!")
-        except json.JSONDecodeError:
-            st.error("Failed to parse the response as JSON.")
-            st.text(response.text)
+# Example usage in your Streamlit file uploader section
+if uploaded_file is not None:
+    if uploaded_file.name.endswith('.zip'):
+        # Process Zip file
+        metadata = extract_metadata_from_index(uploaded_file)
+        if metadata:
+            st.write("Metadata extracted:", metadata)
+        else:
+            st.error("No index.html file found in the zip.")
+        
+        # [Continue with OGG file extraction and processing as before]
