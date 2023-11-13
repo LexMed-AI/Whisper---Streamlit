eb7f1440b0663f71c89bd205f96cbaa635f192aa
flyhogz <45820958+flyhogz@users.noreply.github.com>
2023-11-13 12:09:23 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,16 +14,15 @@ logo_image = Image.open('lexmed_logo.png')
 st.image(logo_image, width=800)
 st.title('Hearing Whisperer')
 
-# Function to add speaker labels to the transcript
-def add_speaker_labels(transcript):
-    # Add metadata at the beginning of the transcript
+# Function to add speaker labels and metadata to the transcript
+def add_speaker_labels_and_metadata(transcript, metadata):
     processed_text = f"Title: TRANSCRIPT of the Social Security Disability Hearing for {metadata['claimant_name']}\n"
     processed_text += f"Claimant: {metadata['claimant_name']}\n"
     processed_text += f"Administrative Law Judge: {metadata['judge_name']}\n"
     processed_text += f"Appearances: {', '.join(metadata['appearances'])}\n\n"
-
+    
     # Add timestamps and process the rest of the transcript
-    for line in srt_text.split('\n'):
+    for line in transcript.split('\n'):
         if '-->' in line:  # Identify timestamp lines
             timestamp = line.strip()
             processed_text += f"[{timestamp}] "  # Add timestamp to the transcript
@@ -32,18 +31,78 @@ def add_speaker_labels(transcript):
 
     return processed_text
 
-# Example usage
-metadata = {
-    "claimant_name": "John Doe",
-    "judge_name": "Jane Smith",
-    "appearances": ["John Doe Claimant", "Jane Lawyer Attorney for Claimant", "Alex Expert Vocational Expert"]
-}
-srt_transcription_processed = preprocess_transcript(srt_transcription, metadata)
+# Function to convert SRT text to PDF
+def srt_to_pdf(srt_text, file_name):
+    pdf_buffer = BytesIO()
+    c = canvas.Canvas(pdf_buffer, pagesize=letter)
+    c.setTitle(file_name)
+
+    y_position = 750
+    for line in srt_text.split('\n'):
+        if not line.strip().isdigit() and line.strip() != '':
+            c.drawString(72, y_position, line)
+            y_position -= 15
+            if y_position < 72:
+                y_position = 750
+                c.showPage()
+
+    c.save()
+    pdf_buffer.seek(0)
+    return pdf_buffer
+
+# Function to extract metadata from zip file
+def extract_metadata(zip_file):
+    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+        metadata_file = [f for f in zip_ref.namelist() if f.endswith('.json')]
+        if metadata_file:
+            metadata_content = zip_ref.read(metadata_file[0])
+            return json.loads(metadata_content)
+        else:
+            return None
+
+# Streamlit file uploader
+uploaded_file = st.file_uploader("Upload your Zip or OGG audio file", type=['zip', 'ogg'])
+
+# Rest of the code for file processing
+# ...
+
+# Example usage (within the appropriate section of the file processing code)
+if uploaded_file is not None and uploaded_file.name.endswith('.ogg'):
+    # Process OGG file
+    # ...
+    srt_transcription = response.json()['text']
+    metadata = {
+        "claimant_name": "John Doe",
+        "judge_name": "Jane Smith",
+        "appearances": ["John Doe Claimant", "Jane Lawyer Attorney for Claimant", "Alex Expert Vocational Expert"]
+    }
+    srt_transcription_processed = add_speaker_labels_and_metadata(srt_transcription, metadata)
+    pdf_file_buffer = srt_to_pdf(srt_transcription_processed, 'Transcription.pdf')
+    # ...
+
 
 
 # Function to convert SRT text to PDF
 def srt_to_pdf(srt_text, file_name):
-    # [Existing code for srt_to_pdf remains unchanged]
+   def srt_to_pdf(srt_text, file_name):
+    pdf_buffer = BytesIO()
+    c = canvas.Canvas(pdf_buffer, pagesize=letter)
+    c.setTitle(file_name)
+       ```
+y_position = 750
+for line in srt_text.split('\\n'):
+    if not line.strip().isdigit() and line.strip() != '':
+        c.drawString(72, y_position, line)
+        y_position -= 15
+        if y_position < 72:
+            y_position = 750
+            c.showPage()
+
+c.save()
+pdf_buffer.seek(0)
+return pdf_buffer
+
+```
 
 # Function to extract metadata from zip file
 def extract_metadata(zip_file):
