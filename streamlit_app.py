8b76e140e8d22a905922582e8b82c8c847818358
flyhogz <45820958+flyhogz@users.noreply.github.com>
2023-11-13 18:26:16 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -117,22 +117,20 @@ def enhanced_process_ogg_file(uploaded_file):
             timeout=60  # Set a reasonable timeout for the request
         )
         if response.status_code == 200:
-            try:
-                return response.json()['text']
-            except json.JSONDecodeError:
-                # Log or print the raw response for debugging
-                logging.error(f"JSON parsing error. Response content: {response.content}")
-                st.error("Failed to parse the response as JSON. Please check the logs.")
-                return None
+            return response.json()['text']
         else:
-            # ... existing error handling ...
-            
+            st.error(f"Failed to transcribe audio. Status code: {response.status_code}. Response text: {response.text}")
+            return None
     except requests.exceptions.Timeout:
-        # ... existing error handling ...
+        st.error("The transcription request timed out. Please try again.")
     except requests.exceptions.RequestException as e:
-        # ... existing error handling ...
+        st.error(f"A network error occurred: {e}")
+    except json.JSONDecodeError:
+        st.error("Failed to parse the response as JSON.")
     except Exception as e:
-        # ... existing error handling ...
+        st.error(f"An unexpected error occurred: {e}")
+        logging.error(f"OGG file processing error: {e}")
+        return None
 
 # Main function of the script
 def main():
